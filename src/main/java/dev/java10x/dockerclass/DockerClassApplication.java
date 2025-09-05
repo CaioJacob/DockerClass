304dae95f6ae0dfcab4b47866c6f593c71661a93
Caio Jacob <147404364+CaioJacob@users.noreply.github.com>
2025-09-05 11:22:32 +0100

    Initial commit: Spring Boot project setup for Docker class

---
+++ src/main/java/dev/java10x/dockerclass/DockerClassApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.dockerclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerClassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/java10x/dockerclass/DockerClassApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.dockerclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerClassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
