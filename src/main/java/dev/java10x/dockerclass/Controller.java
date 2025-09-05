cd66a24fa9cf50d36409a38086993310714d92fd
Caio Jacob <147404364+CaioJacob@users.noreply.github.com>
2025-09-05 12:30:02 +0100

    chore: add Dockerfile and update Controller.java

--- src/main/java/dev/java10x/dockerclass/Controller.java
+++ src/main/java/dev/java10x/dockerclass/Controller.java
@@ -1,4 +1,17 @@
 package dev.java10x.dockerclass;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class Controller {
+
+    @GetMapping("/docker")
+    public String message(){
+        return "This is a Docker application";
+    }
+
+
 }
